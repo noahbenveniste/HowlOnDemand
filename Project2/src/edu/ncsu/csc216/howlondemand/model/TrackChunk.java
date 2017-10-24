b1c3bb0c989861e58568707b9b07e40c7989768b
noahbenveniste <benven.noa1995@gmail.com>
2017-10-24 10:13:32 -0400

    Completed initial implementation of TrackChunk
    unit test status: untested

--- Project2/src/edu/ncsu/csc216/howlondemand/model/TrackChunk.java
+++ Project2/src/edu/ncsu/csc216/howlondemand/model/TrackChunk.java
@@ -11,10 +11,10 @@ public class TrackChunk {
 	private String chunk;
 	
 	/**
-	 * 
+	 * Default constructor for the TrackChunk
 	 */
 	public TrackChunk() {
-		
+		//TODO check that this is supposed to just be empty
 	}
 	
 	/**
@@ -28,7 +28,7 @@ public class TrackChunk {
 	
 	/**
 	 * 
-	 * @param chunk
+	 * @param chunk the 
 	 * @throws MalformedTrackException 
 	 */
 	public void setChunk(String chunk) throws MalformedTrackException {
@@ -40,21 +40,36 @@ public class TrackChunk {
 	}
 	
 	/**
-	 * 
+	 * Returns the chunk field
+	 * @return the chunk field
 	 */
 	public String getChunk() {
 		return this.chunk;
 	}
 	
 	/**
-	 * 
-	 * @param chunk
+	 * Used to validate a chunk string. A chunk string is valid only if it is exactly 8 characters, with those
+	 * characters only consisting of valid hexadecimal digits i.e. 0-9,A-F
+	 * @param chunk the track chunk to validate
+	 * @return true if the chunk is valid, false if the input is null or it does not conform to the standards described above.
 	 */
 	public boolean validChunk(String chunk) {
+		String regex = "([0-9]||[A-F]){8}"; //Indicates that the chunk must contain exactly 8 characters that can range from 0-9,A-F
 		if (chunk == null) {
 			return false;
-		} else if () {
-			
+		} else if (!chunk.matches(regex)) {
+			return false;
+		} else {
+			return true;
 		}
+	}
+	
+	/**
+	 * Returns a string representation of the TrackChunk
+	 * @return a string representation of the TrackChunk
+	 */
+	public String toString() {
+		//TODO make sure that this format is correct
+		return this.chunk;
 	}
 }
