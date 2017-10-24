f880c0607cf88e80d879e1fa75a742523a7b9ba0
noahbenveniste <benven.noa1995@gmail.com>
2017-10-24 11:57:27 -0400

    Added junit to build path, created TrackChunkTest

---
+++ Project2/test/edu/ncsu/csc216/howlondemand/model/TrackChunkTest.java
@@ -0,0 +1,61 @@
+package edu.ncsu.csc216.howlondemand.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Test class for TrackChunk
+ * @author Noah Benveniste
+ */
+public class TrackChunkTest {
+
+	/**
+	 * Test method for TrackChunk() constructor
+	 */
+	@Test
+	public void testTrackChunk() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for TrackChunk(String) constructor
+	 */
+	@Test
+	public void testTrackChunkString() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for validChunk()
+	 */
+	@Test
+	public void testValidChunk() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for setChunk()
+	 */
+	@Test
+	public void testSetChunk() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for getChunk()
+	 */
+	@Test
+	public void testGetChunk() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for toString()
+	 */
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+}
