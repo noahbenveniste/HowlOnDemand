14c7b183d6ab3a394204f1f553f961bd37c92a56
noahbenveniste <benven.noa1995@gmail.com>
2017-10-24 14:10:55 -0400

    Created a skeleton for AudioTrackTest

---
+++ Project2/test/edu/ncsu/csc216/howlondemand/model/AudioTrackTest.java
@@ -0,0 +1,132 @@
+package edu.ncsu.csc216.howlondemand.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Unit tests for AudioTrack its inherited methods from Multimedia
+ * @author Noah Benveniste
+ */
+public class AudioTrackTest {
+
+	/**
+	 * Test method for {@link edu.ncsu.csc216.howlondemand.model.AudioTrack#AudioTrack(int, java.lang.String, java.lang.String)}.
+	 */
+	@Test
+	public void testAudioTrackIntStringString() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.ncsu.csc216.howlondemand.model.AudioTrack#AudioTrack(edu.ncsu.csc216.audioxml.xml.AudioTrackXML)}.
+	 */
+	@Test
+	public void testAudioTrackAudioTrackXML() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.ncsu.csc216.howlondemand.model.AudioTrack#getTrackChunkSize()}.
+	 */
+	@Test
+	public void testGetTrackChunkSize() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.ncsu.csc216.howlondemand.model.Multimedia#getId()}.
+	 */
+	@Test
+	public void testGetId() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.ncsu.csc216.howlondemand.model.Multimedia#setId(int)}.
+	 */
+	@Test
+	public void testSetId() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.ncsu.csc216.howlondemand.model.Multimedia#getTitle()}.
+	 */
+	@Test
+	public void testGetTitle() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.ncsu.csc216.howlondemand.model.Multimedia#setTitle(java.lang.String)}.
+	 */
+	@Test
+	public void testSetTitle() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.ncsu.csc216.howlondemand.model.AudioTrack#getArtist()}.
+	 */
+	@Test
+	public void testGetArtist() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.ncsu.csc216.howlondemand.model.AudioTrack#setArtist(java.lang.String)}.
+	 */
+	@Test
+	public void testSetArtist() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.ncsu.csc216.howlondemand.model.AudioTrack#getChunkIndex()}.
+	 */
+	@Test
+	public void testGetChunkIndex() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.ncsu.csc216.howlondemand.model.AudioTrack#setChunkIndex(int)}.
+	 */
+	@Test
+	public void testSetChunkIndex() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.ncsu.csc216.howlondemand.model.AudioTrack#addChunk(edu.ncsu.csc216.howlondemand.model.TrackChunk)}.
+	 */
+	@Test
+	public void testAddChunk() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.ncsu.csc216.howlondemand.model.AudioTrack#hasNextChunk()}.
+	 */
+	@Test
+	public void testHasNextChunk() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.ncsu.csc216.howlondemand.model.AudioTrack#getNextChunk()}.
+	 */
+	@Test
+	public void testGetNextChunk() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link edu.ncsu.csc216.howlondemand.model.AudioTrack#toString()}.
+	 */
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+}
