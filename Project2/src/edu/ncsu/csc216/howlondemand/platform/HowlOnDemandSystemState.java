48a471d9cb49189e155dd9d649939143d6ffe86d
noahbenveniste <benven.noa1995@gmail.com>
2017-10-26 11:58:21 -0400

    Added javadoc to the HowlOnDemandSystemState interface

--- Project2/src/edu/ncsu/csc216/howlondemand/platform/HowlOnDemandSystemState.java
+++ Project2/src/edu/ncsu/csc216/howlondemand/platform/HowlOnDemandSystemState.java
@@ -2,6 +2,11 @@ package edu.ncsu.csc216.howlondemand.platform;
 
 import edu.ncsu.csc216.howlondemand.platform.enums.CommandValue;
 
+/**
+ * Interface that provides the behavior that the HowlOnDemandSystem FSM state inner classes
+ * should implement
+ * @author Noah Benveniste
+ */
 public interface HowlOnDemandSystemState {
 	/**
 	 * Update the {@link HowlOnDemandSystem} based on the given {@link Command}.
@@ -14,5 +19,9 @@ public interface HowlOnDemandSystemState {
 	 */
 	void updateState(Command c);
 	
+	/**
+	 * Gets the name of the state as a string
+	 * @return the name of the state as a string
+	 */
 	String getStateName();
 }
