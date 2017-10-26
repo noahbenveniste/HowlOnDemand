3015d9e6153816a3b591e09e2e80393e97507db4
noahbenveniste <benven.noa1995@gmail.com>
2017-10-26 10:10:46 -0400

    Created skeleton unit test files for Command and CommandValue

---
+++ Project2/test/edu/ncsu/csc216/howlondemand/platform/CommandTest.java
@@ -0,0 +1,38 @@
+package edu.ncsu.csc216.howlondemand.platform;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Unit tests for the Command class
+ * @author Noah Benveniste
+ */
+public class CommandTest {
+
+	/**
+	 * 
+	 */
+	@Before
+	public void setUp() {}
+	
+	/**
+	 * 
+	 */
+	@Test
+	public void testCommand() {}
+	
+	/**
+	 * 
+	 */
+	@Test
+	public void testGetCommand() {}
+	
+	/**
+	 * 
+	 */
+	@Test
+	public void testToString() {}
+	
+}
---
+++ Project2/test/edu/ncsu/csc216/howlondemand/platform/enums/CommandValueTest.java
@@ -0,0 +1,21 @@
+package edu.ncsu.csc216.howlondemand.platform.enums;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * 
+ * @author Noah Benveniste
+ */
+public class CommandValueTest {
+
+	/**
+	 * 
+	 */
+	@Test
+	public void testCommandValue() {
+		fail("Not yet implemented");
+	}
+	
+}
