b9989ed4ea54bc5d0fad9ac393d438e3895a9696
noahbenveniste <benven.noa1995@gmail.com>
2017-10-27 16:05:58 -0400

    Added descriptions to javadoc fields in HowlOnDemandSystemTest

--- Project2/test/edu/ncsu/csc216/howlondemand/platform/HowlOnDemandSystemTest.java
+++ Project2/test/edu/ncsu/csc216/howlondemand/platform/HowlOnDemandSystemTest.java
@@ -5,47 +5,51 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
+import edu.ncsu.csc216.audioxml.xml.MalformedTrackException;
+import edu.ncsu.csc216.audioxml.xml.StationIOException;
+
 /**
  * Unit tests for HowlOnDemandSystem
  * @author Noah Benveniste
  */
 public class HowlOnDemandSystemTest {
-	/** */
+	/** Path for file containing invalid data */
 	private static final String INVALID_FILE = "test-files/InvalidSimpleStation.xml";
-	/** */
+	/** Path for file containing valid data */
 	private static final String VALID_FILE = "test-files/StationList_short.xml";
 	
-	/** */
+	/** The name of the SelectionState */
 	public static final String SELECTION_NAME = "Selection";
-	/** */
+	/** The name of the PlayWithBufferingState */
 	public static final String PLAYWITHBUFFERING_NAME = "Playing with Buffering";
-	/** */
+	/** The name of the PlayWithoutBufferingState */
 	public static final String PLAYWITHOUTBUFFERING_NAME = "Playing without Buffering";
-	/** */
+	/** The name of the StopWithBufferingState */
 	public static final String STOPWITHBUFFERING_NAME = "Stopped with Buffering";
-	/** */
+	/** The name of the StopWithoutBufferingState */
 	public static final String STOPWITHOUTBUFFERING_NAME = "Stopped without Buffering";
-	/** */
+	/** The name of the QuitState */
 	public static final String QUIT_NAME = "Quit";
-	/** */
+	/** The name of the FinishedState */
 	public static final String FINISHED_NAME = "Finished";
 	
 	/** */
 	private HowlOnDemandSystem h;
 	
 	/**
-	 * 
+	 * Used to reset the singleton instance before each test case
 	 */
 	@Before
 	public void setUp() {
+		h = HowlOnDemandSystem.getInstance();
+		h.reset();
 	}
 
 	/**
-	 * Test method for {@link edu.ncsu.csc216.howlondemand.platform.HowlOnDemandSystem#getInstance()}.
+	 * Test method for getInstance()
 	 */
 	@Test
 	public void testGetInstance() {
-		h = HowlOnDemandSystem.getInstance();
 		assertNull(h.getCurrentStation());
 		assertEquals(0, h.getStations().size());
 		assertEquals(SELECTION_NAME, h.getState().getStateName());
@@ -53,15 +57,38 @@ public class HowlOnDemandSystemTest {
 	}
 
 	/**
-	 * Test method for {@link edu.ncsu.csc216.howlondemand.platform.HowlOnDemandSystem#loadStationsFromFile(java.lang.String)}.
+	 * Test method for loadStationsFromFile()
 	 */
 	@Test
 	public void testLoadStationsFromFile() {
-		fail();
+		//Try loading a station that has tracks with bad chunk data
+		try {
+			h.loadStationsFromFile(INVALID_FILE);
+			fail();
+		} catch (MalformedTrackException e) {
+			fail(); //Shouldn't be thrown
+		} catch (StationIOException e) {
+			fail();
+		} catch (IllegalArgumentException e) {
+			assertEquals(0, h.getStations().size());
+			assertEquals("Bad chunk data", e.getMessage());
+		}
+		
+		//Try loading a valid station
+		try {
+			h.loadStationsFromFile(VALID_FILE);
+		} catch (MalformedTrackException e) {
+			fail(); //Shouldn't be thrown
+		} catch (StationIOException e) {
+			fail();
+		} catch (IllegalArgumentException e) {
+			fail();
+		}
+		assertEquals(9, h.getStations().size());
 	}
 
 	/**
-	 * Test method for {@link edu.ncsu.csc216.howlondemand.platform.HowlOnDemandSystem#getStations()}.
+	 * Test method for getStations()
 	 */
 	@Test
 	public void testGetStations() {
@@ -69,15 +96,24 @@ public class HowlOnDemandSystemTest {
 	}
 
 	/**
-	 * Test method for {@link edu.ncsu.csc216.howlondemand.platform.HowlOnDemandSystem#loadStation(edu.ncsu.csc216.howlondemand.model.Station)}.
+	 * Test method for loadStation()
 	 */
 	@Test
 	public void testLoadStation() {
-		fail("Not yet implemented");
+		//Load the station data
+		try {
+			h.loadStationsFromFile(VALID_FILE);
+		} catch (MalformedTrackException | StationIOException e) {
+			fail();
+		}
+		
+		//Set the first station in the station collection as the current station
+		h.loadStation(h.getStations().get(0));
+		assertEquals(h.getStations().get(0), h.getCurrentStation());
 	}
 
 	/**
-	 * Test method for {@link edu.ncsu.csc216.howlondemand.platform.HowlOnDemandSystem#getCurrentStation()}.
+	 * Test method for getCurrentStation()
 	 */
 	@Test
 	public void testGetCurrentStation() {
@@ -85,7 +121,7 @@ public class HowlOnDemandSystemTest {
 	}
 
 	/**
-	 * Test method for {@link edu.ncsu.csc216.howlondemand.platform.HowlOnDemandSystem#getCurrentAudioTrack()}.
+	 * Test method for getCurrentAudioTrack()
 	 */
 	@Test
 	public void testGetCurrentAudioTrack() {
